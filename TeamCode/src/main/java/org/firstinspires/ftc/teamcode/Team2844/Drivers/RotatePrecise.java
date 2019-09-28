af2331f6ced0fd6bf66f70ffb89cb1cb3b651c6a
Samatron <smadderom62@gmail.com>
2019-09-28 11:51:07 -0700

    testing precise rotate and  adding bogus first imu read

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/Drivers/RotatePrecise.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/Drivers/RotatePrecise.java
@@ -20,26 +20,64 @@ public class RotatePrecise
         runtime_ = new ElapsedTime();
         waiting_ = false;
 
+        /* ---new remapping code --*/
+        //swapping y & z axis due to vertical mounting of rev expansion board
+        //byte AXIS_MAP_CONFIG_BYTE = 0x6; //This is what to write to the AXIS_MAP_CONFIG register to swap x and z axes
+        byte AXIS_MAP_CONFIG_BYTE = 0x18; //This is what to write to the AXIS_MAP_CONFIG register to swap y and z axes
+        byte AXIS_MAP_SIGN_BYTE = 0x1; //This is what to write to the AXIS_MAP_SIGN register to negate the z axis
+
+        //Need to be in CONFIG mode to write to registers
+        robot_.imu.write8(BNO055IMU.Register.OPR_MODE,BNO055IMU.SensorMode.CONFIG.bVal);
+
+        robot_.OpMode_.sleep(100); //Changing modes requires a delay before doing anything else
+
+        //Write to the AXIS_MAP_CONFIG register
+        robot_.imu.write8(BNO055IMU.Register.AXIS_MAP_CONFIG,AXIS_MAP_CONFIG_BYTE);
+
+        //Write to the AXIS_MAP_SIGN register
+        robot_.imu.write8(BNO055IMU.Register.AXIS_MAP_SIGN,AXIS_MAP_SIGN_BYTE);
+
+        //Need to change back into the IMU mode to use the gyro
+        robot_.imu.write8(BNO055IMU.Register.OPR_MODE,BNO055IMU.SensorMode.IMU.bVal & 0x0F);
+
+        robot_.OpMode_.sleep(100); //Changing modes again requires a delay
+        /* ---new remapping code ---*/
+
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters(); // inertial motion unit
 
         parameters.mode                = BNO055IMU.SensorMode.IMU;
         parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
         parameters.loggingEnabled      = false;
+
+        robot_.imu.initialize(parameters);
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!robot_.OpMode_.isStopRequested() && !robot_.imu.isGyroCalibrated())
+        {
+            robot_.OpMode_.sleep(50);
+            robot.OpMode_.idle();
+        }
+        if (!robot_.imu.isGyroCalibrated()) {
+            System.out.println("ValleyX: Gyro not calibrated");
+        }
+
+        System.out.println("ValleyX: imu calib status" + robot_.imu.getCalibrationStatus().toString());
+        robot_.OpMode_.telemetry.addData("Mode", "calibrated");
+        robot_.OpMode_.telemetry.update();
+        float gyroActual = robot_.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+
     }
 
-
     // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
-
     // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
-
     // and named "imu".
-
 
     public void RotatePrecise(double gyroTarget, double gyroRange, double minSpeed, double addSpeed, int timesCorrect)
     {
         double turnPower = 0;
         double gyroActual = robot_.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        gyroTarget = -gyroTarget;
         gyroTarget += gyroActual + 360.0;
         gyroTarget %= 360;
         int correctCount = 0;
@@ -52,6 +90,8 @@ public class RotatePrecise
 
 
             if (delta > 180.0) delta -= 360.0; //makes delta between -180 and 180
+
+            System.out.println("ValleyX delta value " + delta);
             if (Math.abs(delta) > gyroRange)
             { //checks if delta is out of range
                 correctCount = 0;
@@ -65,7 +105,8 @@ public class RotatePrecise
                 turnPower = 0;
             }
             double  leftPower, rightPower;
-            if (gyroTarget < 0)
+            System.out.println("ValleyX gyroTarget " + gyroTarget);
+            if (gyroTarget > 0)
             {   // turn right
                 leftPower = -turnPower;
                 rightPower = turnPower;
@@ -81,6 +122,7 @@ public class RotatePrecise
             robot_.leftBackDrive.setPower(leftPower);
             robot_.rightFrontDrive.setPower(rightPower);
             robot_.rightBackDrive.setPower(rightPower);
+            robot_.OpMode_.idle();
         }
         //   return this.correctCount;
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/TestDrivers/TestRotatePrecise.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/TestDrivers/TestRotatePrecise.java
@@ -21,11 +21,44 @@ public class TestRotatePrecise extends LinearOpMode
         RobotHardware robot = new RobotHardware(hardwareMap, this);
         RotatePrecise rotatePrecise = new RotatePrecise(robot);
 
+        //sleep(2000);
         System.out.println("ValleyX: Starting...");
         System.out.println("ValleyX: Rotate 90 degrees");
 
+        telemetry.addData("gyrotarget ", 90);
+        telemetry.update();
         rotatePrecise.RotatePrecise(90, 1, 0.2, 0.3, 5);
-    }
+        sleep(2000);
 
+        telemetry.addData("gyrotarget ", -90);
+        telemetry.update();
+        rotatePrecise.RotatePrecise(-90, 1, 0.2, 0.3, 5);
+        sleep(2000);
+
+        telemetry.addData("gyrotarget ", 270);
+        telemetry.update();
+        rotatePrecise.RotatePrecise(270, 1, 0.2, 0.3, 5);
+        sleep(2000);
+
+        telemetry.addData("gyrotarget ", 45);
+        telemetry.update();
+        rotatePrecise.RotatePrecise(45, 1, 0.2, 0.3, 5);
+        sleep(2000);
+
+        telemetry.addData("gyrotarget ", 15);
+        telemetry.update();
+        rotatePrecise.RotatePrecise(15, 1, 0.2, 0.3, 5);
+        sleep(2000);
+
+        telemetry.addData("gyrotarget ", 20);
+        telemetry.update();
+        rotatePrecise.RotatePrecise(20, 1, 0.2, 0.3, 5);
+        sleep(2000);
+
+        telemetry.addData("gyrotarget ", 10);
+        telemetry.update();
+        rotatePrecise.RotatePrecise(10, 1, 0.2, 0.3, 5);
+        sleep(2000);
+    }
 }
 
