632382a83cb02cca41f696a4a6686080d9eb3d5c
valle <jairugar57@gmail.com>
2019-11-09 10:41:09 -0700

    Produced working code for detecting yellow blocks. Detects color on the field. Converted to Class, functions well.
    Finalized class details for sensing blocks and color for yellow blocks or others.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/Drivers/ColorDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/Drivers/ColorDriver.java
@@ -9,21 +9,22 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 
 public class ColorDriver {
     private PushbotRobotHardware robot_;
-    //private ColorSensor senseColor;
-
+    
     public ColorDriver(PushbotRobotHardware robot) {
         robot_ = robot;
 
     }
 
+    public boolean isSeen() {
+        return  !new Double(robot_.distancedriver.getDistance(DistanceUnit.MM)).isNaN();
+    }
 
     public boolean isYellow() {
-
-        //Maps Sensor to the REV Hub
-        //senseColor = hardwareMap.get(ColorSensor.class, "Color_Sensor");
 
         //insert color hue test
         float hsvValues[] = {0F, 0F, 0F};
@@ -32,27 +33,11 @@ public class ColorDriver {
 
         final double SCALE_FACTOR = 255;
 
-       // int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
-       // final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+        Color.RGBToHSV((int) (robot_.colordriver.red() * SCALE_FACTOR),
+                (int) (robot_.colordriver.green() * SCALE_FACTOR),
+                (int) (robot_.colordriver.blue() * SCALE_FACTOR),
+                hsvValues);
 
-        //waitForStart();
-
-        //while (opModeIsActive()) {
-            Color.RGBToHSV((int) (robot_.colordriver.red() * SCALE_FACTOR),
-                    (int) (robot_.colordriver.green() * SCALE_FACTOR),
-                    (int) (robot_.colordriver.blue() * SCALE_FACTOR),
-                    hsvValues);
-            /*
-            robot_.OpMode_.telemetry.addData("isYellow", hsvValues[0] >= 40 && hsvValues[0] <= 70);
-
-            robot_.OpMode_.telemetry.addData("Red", robot_.colordriver.red());
-            robot_.OpMode_.telemetry.addData("Blue", robot_.colordriver.blue());
-            robot_.OpMode_.telemetry.addData("Green", robot_.colordriver.green());
-            robot_.OpMode_.telemetry.addData("Hue", hsvValues[0]);
-
-            robot_.OpMode_.telemetry.update();
-
-             */
-            return (hsvValues[0] >= 40 && hsvValues[0] <= 70);
+        return (hsvValues[0] >= 40 && hsvValues[0] <= 70);
     }
 }
