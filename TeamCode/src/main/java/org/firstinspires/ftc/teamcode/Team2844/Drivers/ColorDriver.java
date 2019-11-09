f2b74f310940013f2937b07c33e29d09716b5a2d
valle <jairugar57@gmail.com>
2019-11-08 18:16:27 -0700

    Produced working code for detecting yellow blocks. Detects color on the field. Converted to Class, functions well.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/Drivers/ColorDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/Drivers/ColorDriver.java
@@ -4,21 +4,26 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.view.View;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@Autonomous(name = "ColorDistanceSensor", group = "Sensor")
-public class ColorDriver extends LinearOpMode{
 
+public class ColorDriver {
     private PushbotRobotHardware robot_;
+    //private ColorSensor senseColor;
 
-    private ColorSensor senseColor;
-    @Override
-    public void runOpMode() throws InterruptedException {
+    public ColorDriver(PushbotRobotHardware robot) {
+        robot_ = robot;
+
+    }
+
+
+    public boolean isYellow() {
 
         //Maps Sensor to the REV Hub
-        senseColor = hardwareMap.get(ColorSensor.class, "Color_Sensor");
+        //senseColor = hardwareMap.get(ColorSensor.class, "Color_Sensor");
 
         //insert color hue test
         float hsvValues[] = {0F, 0F, 0F};
@@ -27,29 +32,27 @@ public class ColorDriver extends LinearOpMode{
 
         final double SCALE_FACTOR = 255;
 
-        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
-        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+       // int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+       // final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
 
-        waitForStart();
+        //waitForStart();
 
-        while (opModeIsActive()) {
-            Color.RGBToHSV((int) (senseColor.red() * SCALE_FACTOR),
-                    (int) (senseColor.green() * SCALE_FACTOR),
-                    (int) (senseColor.blue() * SCALE_FACTOR),
+        //while (opModeIsActive()) {
+            Color.RGBToHSV((int) (robot_.colordriver.red() * SCALE_FACTOR),
+                    (int) (robot_.colordriver.green() * SCALE_FACTOR),
+                    (int) (robot_.colordriver.blue() * SCALE_FACTOR),
                     hsvValues);
+            /*
+            robot_.OpMode_.telemetry.addData("isYellow", hsvValues[0] >= 40 && hsvValues[0] <= 70);
 
-            senseColor.red();
-            senseColor.blue();
-            senseColor.green();
+            robot_.OpMode_.telemetry.addData("Red", robot_.colordriver.red());
+            robot_.OpMode_.telemetry.addData("Blue", robot_.colordriver.blue());
+            robot_.OpMode_.telemetry.addData("Green", robot_.colordriver.green());
+            robot_.OpMode_.telemetry.addData("Hue", hsvValues[0]);
 
-            telemetry.addData("isYellow", hsvValues[0] >= 40 && hsvValues[0] <= 70);
+            robot_.OpMode_.telemetry.update();
 
-            telemetry.addData("Red", senseColor.red());
-            telemetry.addData("Blue", senseColor.blue());
-            telemetry.addData("Green", senseColor.green());
-            telemetry.addData("Hue", hsvValues[0]);
-
-            telemetry.update();
-        }
+             */
+            return (hsvValues[0] >= 40 && hsvValues[0] <= 70);
     }
 }
